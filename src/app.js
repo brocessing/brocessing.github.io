c9c03152c99b3bdd780e635e165c9a72f11fb31a
Léo Ternoir <leo@leoternoir.com>
2017-08-31 12:18:49 +0200

    Add 1st versionn of website

--- src/app.js
+++ src/app.js
@@ -1,1 +1,1 @@
-console.log(`It's working !`)
+console.log(`💪 Prot shake !`)
--- src/pages.js
+++ src/pages.js
@@ -1,7 +1,20 @@
 const contents = {
-  title: 'bro-start',
-  description: 'Blazing fast webpack setup for static websites',
-  message: 'Everything is working.\nHave fun!'
+  title: `Brocessing`,
+  description: `Angry creative tech bureau based between Paris and Strasbourg`,
+  email: `b0ss@brocessing.men`,
+  url: `brocessing.men`,
+  thumbnail: `thumbnail.png`,
+  message: 'Angry creative tech bureau\nBased between Paris and Strasbourg',
+  contact: [
+    {
+      name: `Github`,
+      url: `https://github.com/brocessing`
+    },
+    {
+      name: `Mail`,
+      url: `mailto:b0ss@brocessing.men`
+    }
+  ]
 }
 
 const pages = [
