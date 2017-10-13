566306fb75c8d567fd9cc3c9547eef349e2ee8ed
antoine1000 <bonjour@antoine.cool>
2017-10-13 17:47:59 +0200

    Add a "growcessing" page for displaying some projects

--- src/pages.js
+++ src/pages.js
@@ -21,11 +21,43 @@ const contents = {
   ]
 }
 
+const links = {
+  title: `Projets sous stéroïdes`,
+
+  links: [
+    {
+      name: `Eyebro`,
+      url: `https://brocessing.github.io/eyebro/`,
+      caption: `Un runner game qui se joue avec les sourcils`
+    },
+    {
+      name: `AR Music Experiment`,
+      url: ` https://twitter.com/hugopiquemal/status/892807824908136449`,
+      caption: `Générateur de boucles musicales via réalité augmentée`
+    },
+    {
+      name: `Arnaud Juracek [Portfolio]`,
+      url: `http://arnaudjuracek.fr`,
+      caption: `Un portfolio à contrôler à plusieurs en même temps`
+    },
+    {
+      name: `Bonus`,
+      url: `puckey-star.png`,
+      caption: `Un like pas comme les autres...`
+    },   
+  ]
+}
+
 const pages = [
   {
     output: 'index.html',
     content: contents,
     layout: 'layouts/index.hbs'
+  },
+  {
+    output: 'growcessing.html',
+    content: links,
+    layout: 'layouts/growcessing.hbs'
   }
 ]
 
