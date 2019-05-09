2207aef3130e6b074b81cedb0633715c9612f922
Léo Ternoir <leo@leoternoir.com>
2019-05-09 14:44:32 +0200

    Remove projects page

--- src/pages.js
+++ src/pages.js
@@ -21,59 +21,11 @@ const contents = {
   ]
 }
 
-const links = {
-  title: `Projets sous stéroïdes`,
-
-  links: [
-    {
-      name: `Eyebro`,
-      url: `https://brocessing.github.io/eyebro/`,
-      caption: `Un runner game qui se joue avec les sourcils`,
-      img: `previews/eyebro.png`,
-      alt: `Preview du jeu Eyebro`
-    },
-    {
-      name: `AR Music Experiment`,
-      url: ` https://twitter.com/hugopiquemal/status/892807824908136449`,
-      caption: `Générateur de boucles musicales via réalité augmentée`,
-      img: `previews/armusic.jpg`,
-      alt: `Preview de l'AR Music Experiment`
-    },
-    {
-      name: `Arnaud Juracek [Portfolio]`,
-      url: `http://arnaudjuracek.fr`,
-      caption: `Un portfolio à contrôler à plusieurs en même temps`,
-      img: `previews/rno.jpg`,
-      alt: `Preview de arnaudjuracek.fr`
-    },
-    {
-      name: `Superflux`,
-      url: `https://antoine.cool/projects/superflux`,
-      caption: `Des objets commandés avec le visage`,
-      img: `previews/superflux.jpg`,
-      alt: `Preview du projet Superflux`
-    }, 
-    {
-      name: `Three.js Neon Experiment`,
-      url: `neon01.mp4`,
-      caption: `Paths SVG convertis en néon 3D dans le navigateur`,
-      video: `previews/neon01.mp4`,
-      video_img: ``,
-      alt: `Vidéo de three.js Neon Experiment`
-    }
-  ]
-}
-
 const pages = [
   {
     output: 'index.html',
     content: contents,
     layout: 'layouts/index.hbs'
-  },
-  {
-    output: 'growcessing.html',
-    content: links,
-    layout: 'layouts/growcessing.hbs'
   }
 ]
 
